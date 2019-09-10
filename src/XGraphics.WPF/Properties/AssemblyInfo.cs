0aa7cc709b0c067dd44fe40d7c3616e424b6659f
Bret Johnson <bret.johnson@microsoft.com>
2019-09-10 03:25:18 -0400

    More build updates

--- src/XGraphics.WPF/Properties/AssemblyInfo.cs
+++ src/XGraphics.WPF/Properties/AssemblyInfo.cs
@@ -22,16 +22,3 @@ using System.Windows.Markup;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("cfc466c2-98c4-4d48-a9ac-97ef60fc96b2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/XGraphics.XamarinForms.iOS/Properties/AssemblyInfo.cs
+++ src/XGraphics.XamarinForms.iOS/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("50c7b8c9-e664-45af-b88e-0c9b8b9c1be1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
