360ba270985bd05871b0d306b7e66fba3f97de6a
Bret Johnson <bret.johnson@microsoft.com>
2019-11-03 21:54:29 -0500

    Moved FillRule and SweepDirection enums to root namespace

---
+++ src/XGraphics/FillRule.cs
@@ -0,0 +1,8 @@
+namespace XGraphics
+{
+    public enum FillRule
+    {
+        EvenOdd = 0,
+        Nonzero = 1
+    }   
+}
---
+++ src/XGraphics/SweepDirection.cs
@@ -0,0 +1,8 @@
+namespace XGraphics
+{
+    public enum SweepDirection
+    {
+        Counterclockwise = 0,
+        Clockwise = 1
+    }   
+}
